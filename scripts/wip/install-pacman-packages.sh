a8d4cf25a6ce16da4f836cef6fe136b11fef739f
Peter Joles <jolzee@gmail.com>
2020-07-02 19:59:38 -0700

    Tweaks

--- scripts/wip/install-pacman-packages.sh
+++ scripts/wip/install-pacman-packages.sh
@@ -2,6 +2,9 @@
 
 echo "Install Arch packages using pacman from list arch-pacman-packages.txt"
 
-sed -r '/^#/d' ../../.backup/arch-pacman-packages.txt | sudo pacman -S --needed -
+#sed -r '/^#/d' ../../.backup/arch-pacman-packages.txt | sudo pacman --needed -S -
+
+installable_packages=$(comm -12 <(pacman -Slq | sort) <(sort ../../.backup/arch-pacman-packages.txt))
+pacman -S --needed $installable_packages
 
 exit 0
